052d19746b94f465f87a6bc93607bdc2ced3f5c9
emyudofia1 <emyudofia1@gmail.com>
2026-01-27 08:16:29 -0700

    Added the missing querry

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -1,3 +1,13 @@
+-- Type: account_type
+
+-- DROP TYPE IF EXISTS public.account_type;
+
+CREATE TYPE public.account_type AS ENUM
+    ('Client', 'Employee', 'Admin');
+
+ALTER TYPE public.account_type
+    OWNER TO cse340eg;
+
 
 --Table structure for table 'classification'
 CREATE TABLE classification (
